a6f0e50492923277dd2e06908787db5a5f394c6b
Ryo Yasuda <e165729@ie.u-ryukyu.ac.jp>
2017-01-21 18:46:53 +0900

    スコアの時間取得完成

---
+++ Assets/Directory/riono/Sprict/Score.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	public Text scorelabel;
+
+
+	private int starttime;
+	private int nowtime;
+	private int duration;
+	private int score;
+
+	// Use this for initialization
+	void Start () {
+
+		starttime = DateTime.Now.Hour * 60 *60 * 1000 + DateTime.Now.Minute * 60 * 1000 + 
+			DateTime.Now.Second * 1000 + DateTime.Now.Millisecond;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		nowtime = DateTime.Now.Hour * 60 *60 * 1000 + DateTime.Now.Minute * 60 * 1000 + 
+			DateTime.Now.Second * 1000 + DateTime.Now.Millisecond;
+
+		duration = nowtime - starttime;
+		score = CalScore ();
+
+		scorelabel.text = "SCORE : " + score;
+	}
+
+	int CalScore(){
+
+		int time = (int)(duration / 1000);
+		int cal = time * 100;
+
+		return cal;
+		}
+}
