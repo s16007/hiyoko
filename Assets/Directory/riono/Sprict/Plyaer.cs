fd09fba60de3d51a454b8e95e6d09e5d4919b306
Ryo Yasuda <e165729@ie.u-ryukyu.ac.jp>
2017-01-21 19:04:49 +0900

    タグ訂正

---
+++ Assets/Directory/riono/Sprict/Plyaer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plyaer : MonoBehaviour {
+		// speedを制御する
+		public float speed = 10;
+
+		void FixedUpdate ()
+		{
+			float x =  Input.GetAxis("Horizontal");
+			float z = Input.GetAxis("Vertical");
+
+			Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+			// xとyにspeedを掛ける
+			rigidbody.AddForce(x * speed, 0, z * speed);
+		}
+	}
