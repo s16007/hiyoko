9c5964e8240b7ec92ed3254e670ec0baea8423f5
toumatakao <e085741@toumatakao-no-MacBook-Pro.local>
2017-01-22 14:19:56 +0900

    main

---
+++ Assets/Main_Resource/Script/SingletonMonoBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : SingletonMonoBehaviour<T> {
+	protected static T instance;
+
+	public static T Instance{
+		get{
+			if(!instance){
+				instance = FindObjectOfType(typeof(T)) as T;
+
+				if(!instance){Debug.LogWarning(typeof(T) + "is nothing");}
+			}
+
+			return instance;
+		}
+	}
+
+	protected void Awake(){
+		CheckInstance();
+	}
+
+	protected bool CheckInstance(){
+		if(!instance){
+			instance = (T)this;
+			return true;
+
+		}else if(Instance == this){
+			return true;
+		}
+
+		Destroy(this);
+		return false;
+	}
+}
